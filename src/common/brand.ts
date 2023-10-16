8dd31cb52281cc5bf91a8b12c68115cdc9684859
Jimmy Liao <jimmyliao@jimmyliao.net>
2023-10-16 11:55:01 +0800

    change sitename

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -8,21 +8,21 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'consulting-bot',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'consulting-bot',
   },
   Meta: {
-    Description: 'Leading open-source AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Harnessing AI for You',
+    Description: 'Consulting Bot, a medical chatbot for doctors and patients',
+    SiteName: 'consulting-bot',
     ThemeColor: '#32383E',
     TwitterSite: '@enricoros',
   },
   URIs: {
-    Home: 'https://big-agi.com',
-    // App: 'https://get.big-agi.com',
+    Home: 'https://jimmyliao.net/consulting-bot',
+    // App: 'https://get.consulting-bot.com',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    // SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
     // Twitter: 'https://www.twitter.com/enricoros',
   },
 };
